1753291024f76c85447b2383239fe99d3f5d3d20
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-23 22:00:16 +0400

    Update Api.ts

--- Frontend/src/Utils/Api.ts
+++ Frontend/src/Utils/Api.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import qs from "qs";
+import authServices from "../Service/Auth";
+import authStore from "../Stores/Auth.store";
 
 const api = axios.create({
   baseURL: "http://localhost:3000",
@@ -7,7 +9,43 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   config.paramsSerializer = (params) => qs.stringify(params, { arrayFormat: "repeat" });
+  const accessToken = authStore.getState().accessToken;
+  if (accessToken) {
+    config.headers["Authorization"] = `Bearer ${accessToken}`;
+  }
   return config;
 });
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (!error.config && error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const refreshToken = authStore.getState().refreshToken;
+        if (!refreshToken) {
+          return Promise.reject(error);
+        }
+
+        const { data } = await authServices.refreshTokens(refreshToken);
+
+        authStore.getState().setTokens(data);
+        originalRequest.headers["Authorization"] = `Bearer ${data.access_token}`;
+
+        return api(originalRequest);
+      } catch (error) {
+        console.log("🚀 ~ error:", error);
+        console.error("Failed to refresh tokens");
+
+        authStore.getState().clearTokens();
+
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 export default api;
