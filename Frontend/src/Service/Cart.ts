f7a2befb566e7f77259cceaf37360f2926c16c63
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-23 14:16:40 +0400

    Services

--- Frontend/src/Service/Cart.ts
+++ Frontend/src/Service/Cart.ts
@@ -2,7 +2,8 @@ import api from "../Utils/Api";
 
 const cartServices = {
   allCartProducts: () => api.get("cart"),
-  deleteCartProduct: (id: string) => api.delete(`cart/${id}`),
+  deleteCartProduct: ({ id, removeAll = false }: { id: string; removeAll?: boolean }) =>
+    api.delete(`cart/${id}`, { params: { removeAll } }),
   clearCart: () => api.post("cart/clear"),
   addCartProduct: (product_id: string) => api.post("cart", { product_id }),
 };
