998e9d4710f6798991888c4e81f27c7aa8aa4de6
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-24 11:01:36 +0400

    Update Root.tsx

--- Frontend/src/Router/Root.tsx
+++ Frontend/src/Router/Root.tsx
@@ -2,10 +2,8 @@ import { Outlet } from "react-router";
 import Header from "../Layout/Header";
 import Footer from "../Layout/Footer";
 import SideBar from "../Layout/SideBar";
-import { useGetCurrentUser } from "../Hooks/useGetCurrentUser";
 
 const Root = () => {
-  useGetCurrentUser();
   return (
     <div>
       <Header />
