1a1a1a9d831783abc840c25958d0c56e43c9d2dd
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-28 22:40:47 +0400

    Update Auth.store.ts

--- Frontend/src/Stores/Auth.store.ts
+++ Frontend/src/Stores/Auth.store.ts
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 import { UserI } from "../Types/User.interface";
+import { jwtDecode } from "jwt-decode";
 
 interface authStoreI {
-  user: UserI | null;
-  setUser: (user: UserI) => void;
+  user: null | UserI;
   accessToken?: string;
   refreshToken?: string;
   setTokens: (token: { access_token: string; refresh_token: string }) => void;
@@ -12,10 +12,17 @@ interface authStoreI {
 
 const authStore = create<authStoreI>()((set) => ({
   user: null,
-  setUser: (user) => set({ user }),
   accessToken: localStorage.getItem("accessToken") || "",
   refreshToken: localStorage.getItem("refreshToken") || "",
   setTokens: (tokens) => {
+    const decodedUser = jwtDecode(tokens.access_token) as UserI;
+    if (!decodedUser) {
+      // TODO: Handle error, maybe redirect to login page, Clear tokens
+      console.error("Invalid token");
+      authStore.getState().clearTokens();
+      return;
+    }
+    set({ user: decodedUser });
     localStorage.setItem("accessToken", tokens.access_token);
     localStorage.setItem("refreshToken", tokens.refresh_token);
     set({ accessToken: tokens.access_token, refreshToken: tokens.refresh_token });
@@ -26,5 +33,17 @@ const authStore = create<authStoreI>()((set) => ({
     set({ accessToken: "", refreshToken: "" });
   },
 }));
+
+const accessToken = localStorage.getItem("accessToken");
+if (accessToken) {
+  try {
+    const decodedUser = jwtDecode<UserI>(accessToken);
+    authStore.setState({ user: decodedUser });
+  } catch (error) {
+    // Handle error if token is invalid
+    console.error("Invalid token:", error);
+    authStore.getState().clearTokens();
+  }
+}
 
 export default authStore;
