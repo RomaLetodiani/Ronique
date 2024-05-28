a2d63ee492964cea74d6c0f35a8134ce6083d31a
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-28 22:41:29 +0400

    Update User.interface.ts

--- Frontend/src/Types/User.interface.ts
+++ Frontend/src/Types/User.interface.ts
@@ -1,12 +1,17 @@
 import { Item } from "./Shared.interface";
 
+export enum Role {
+  ADMIN = "admin",
+  USER = "customer",
+}
+
 export interface UserI extends Item {
   first_name: string;
   last_name: string;
   phone_number: string;
   email: string;
   verified: boolean;
-  role: string;
+  role: Role;
   password: string;
   refresh_token: string;
 }
