903bd1c785810993349b15e323744db11e849680
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-28 22:40:34 +0400

    Update Input.tsx

--- Frontend/src/Components/UI/Input.tsx
+++ Frontend/src/Components/UI/Input.tsx
@@ -8,6 +8,7 @@ interface inputI extends InputHTMLAttributes<HTMLInputElement> {
   wrapperClassName?: string;
   errorMessage?: string;
   hasError?: boolean;
+  focus?: boolean;
 }
 
 const Input = ({
@@ -17,15 +18,23 @@ const Input = ({
   wrapperClassName,
   errorMessage,
   hasError,
+  focus,
   ...rest
 }: inputI) => {
   const [passwordVisible, setPasswordVisible] = useState(false);
   return (
-    <div className={twMerge("", wrapperClassName)}>
+    <div className={twMerge("relative w-full flex flex-col", wrapperClassName)}>
       <label htmlFor={rest.id} className={twMerge("", labelClassName)}>
         {label}
       </label>
-      <input {...rest} className={twMerge("", inputClassName)} />
+      <input
+        {...rest}
+        className={twMerge(
+          "w-full border outline-none px-5 py-3 rounded-2xl",
+          `${hasError && "border-red-500"}`,
+          inputClassName
+        )}
+      />
     </div>
   );
 };
