877a79f7a2d07bbc711c13087698e7f42c84638b
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-30 22:09:10 +0400

    Update Input.tsx

--- Frontend/src/Components/UI/Input.tsx
+++ Frontend/src/Components/UI/Input.tsx
@@ -1,4 +1,6 @@
 import { InputHTMLAttributes, useState } from "react";
+import { GoEyeClosed } from "react-icons/go";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
 import { twMerge } from "tailwind-merge";
 
 interface inputI extends InputHTMLAttributes<HTMLInputElement> {
@@ -22,19 +24,47 @@ const Input = ({
   ...rest
 }: inputI) => {
   const [passwordVisible, setPasswordVisible] = useState(false);
+  const isPassword = rest.type === "password";
   return (
-    <div className={twMerge("relative w-full flex flex-col", wrapperClassName)}>
-      <label htmlFor={rest.id} className={twMerge("", labelClassName)}>
+    <div className={twMerge("relative w-full", wrapperClassName)}>
+      <label
+        htmlFor={rest.id}
+        className={twMerge(
+          "absolute left-3 pointer-events-none transition-all",
+          `${
+            focus
+              ? "top-1 text-sm text-primary"
+              : "transform top-1/2 -translate-y-1/2 text-secondary-200"
+          }`,
+          hasError && "text-error",
+          labelClassName
+        )}
+      >
         {label}
       </label>
       <input
         {...rest}
+        type={isPassword ? (passwordVisible ? "text" : "password") : rest.type}
         className={twMerge(
-          "w-full border outline-none px-5 py-3 rounded-2xl",
-          `${hasError && "border-red-500"}`,
+          "w-full border outline-none px-3 py-1 pt-5 rounded-xl",
+          `${hasError && "border-error"}`,
+          isPassword && "pr-14",
+
           inputClassName
         )}
       />
+      {isPassword && (
+        <div
+          className={twMerge(
+            "absolute cursor-pointer right-0 top-0 text-xl flex justify-center items-center w-[50px] h-[50px]",
+            focus ? "text-primary" : "text-secondary-200",
+            hasError && "text-error"
+          )}
+          onClick={() => setPasswordVisible((prev) => !prev)}
+        >
+          {passwordVisible ? <MdOutlineRemoveRedEye /> : <GoEyeClosed />}
+        </div>
+      )}
     </div>
   );
 };
