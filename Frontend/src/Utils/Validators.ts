acafc8b90256c075930b609922fcc4e34165f4ef
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-30 22:05:36 +0400

    Update Validators.ts

--- Frontend/src/Utils/Validators.ts
+++ Frontend/src/Utils/Validators.ts
@@ -1,1 +1,12 @@
-const emailValidator = (email: string) => {};
+const emailValidator = (email: string) => {
+  const re = /\S+@\S+\.\S+/;
+  return re.test(email);
+};
+const phoneNumberValidator = (phoneNumber: string) => {
+  const re = /\d{10,}/; // Updated regex pattern
+  console.log(re.test(phoneNumber));
+  return phoneNumber.length > 9 && re.test(phoneNumber);
+};
+const isValid = (value: string) => value.length > 6;
+
+export { emailValidator, phoneNumberValidator, isValid };
