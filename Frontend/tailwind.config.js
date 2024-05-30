229e77223a59a8dcaec65a8941b963435d2d5472
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-30 22:03:48 +0400

    Update tailwind.config.js

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -4,11 +4,21 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#FF6363",
+        primary: "#0F3054",
         secondary: {
-          100: "#E2E2D5",
-          200: "#888883",
+          200: "#ace0f9",
+          500: "#3B8DCB",
         },
+        danger: "#FF0000",
+        error: "#FF0000",
+        info: "#FF0000",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      gradientColorStops: {
+        "light-blue": "#ace0f9",
+        "light-gray": "#fff1eb",
       },
     },
   },
