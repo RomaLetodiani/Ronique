f3f9780e254818ead54fd4848063c91cc7147786
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-30 22:09:12 +0400

    Update Button.tsx

--- Frontend/src/Components/UI/Button.tsx
+++ Frontend/src/Components/UI/Button.tsx
@@ -4,14 +4,16 @@ import { twMerge } from "tailwind-merge";
 interface ButtonI extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   className?: string;
+  btnType?: "primary" | "secondary";
 }
 
-const Button = ({ children, className, ...rest }: ButtonI) => {
+const Button = ({ children, className, btnType = "primary", ...rest }: ButtonI) => {
   return (
     <button
       {...rest}
       className={twMerge(
         "rounded-lg cursor-pointer px-5 py-2 shadow-lg hover:opacity-70",
+        btnType === "primary" ? "bg-primary text-white" : "bg-secondary-200 text-primary",
         className
       )}
     >
