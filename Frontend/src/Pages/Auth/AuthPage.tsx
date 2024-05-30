28f56baae49c3b64be9ec8b117f1e461d039001f
RomanLetodiani <roman.letodiani03@gmail.com>
2024-05-30 22:08:14 +0400

    auth page

--- Frontend/src/Pages/Auth/AuthPage.tsx
+++ Frontend/src/Pages/Auth/AuthPage.tsx
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { Outlet, useLocation, useNavigate } from "react-router";
+import { logo } from "../../Components/Shared/Assets/Assets";
+import { FaRegCopyright } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const AuthPage = () => {
   const { pathname } = useLocation();
@@ -10,12 +13,22 @@ const AuthPage = () => {
     }
   }, [pathname]);
   return (
-    <div className="w-full min-h-screen flex flex-col justify-center items-center">
-      <h1>Auth Header</h1>
-      <div className="p-5 border rounded-xl">
-        <Outlet />
-      </div>
-      <h1>Auth Footer</h1>
+    <div className="w-full min-h-screen min-w-[375px] flex flex-col justify-center items-center">
+      <header className="w-full flex justify-center p-5 shadow-lg">
+        <Link to={"/"}>
+          <img src={logo} alt="Ronique Logo" />
+        </Link>
+      </header>
+      <main className="flex-1 flex justify-center items-center">
+        <div className="p-5 border rounded-xl">
+          <Outlet />
+        </div>
+      </main>
+      <footer className="border border-secondary-200 bg-secondary-200 p-5 w-full flex justify-center items-center">
+        <p className="text-center flex items-center gap-2 text-sm text-primary">
+          <FaRegCopyright /> 2024 Ronique
+        </p>
+      </footer>
     </div>
   );
 };
