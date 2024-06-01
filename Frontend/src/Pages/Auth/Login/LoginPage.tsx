84295717ef452cb037e7c9a500ad41d8a2ed2313
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-01 22:42:50 +0400

    Toastify

--- Frontend/src/Pages/Auth/Login/LoginPage.tsx
+++ Frontend/src/Pages/Auth/Login/LoginPage.tsx
@@ -4,31 +4,55 @@ import Input from "../../../Components/UI/Input";
 import authServices from "../../../Services/AuthServices";
 import authStore from "../../../Stores/Auth.store";
 import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { emailValidator, isValid } from "../../../Utils/Validators";
+import { FormEvent } from "react";
 
 const LoginPage = () => {
   const emailInput = useInput(
-    (value) => value.includes("@") || value === import.meta.env.VITE_ADMIN_EMAIL
+    (value) => emailValidator(value) || value === import.meta.env.VITE_ADMIN_EMAIL
   );
-  const passwordInput = useInput((value) => value.length > 6);
+  const passwordInput = useInput((value) => isValid(value));
 
   const { setTokens } = authStore();
 
-  const login = () => {
+  const errors = [
+    emailInput.hasError,
+    passwordInput.hasError,
+    !emailInput.value,
+    !passwordInput.value,
+  ];
+
+  const login = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (errors.some((error) => error)) {
+      toast.error("Please fill all fields correctly");
+      return;
+    }
+
     authServices
       .login({ email: emailInput.value, password: passwordInput.value })
       .then(({ data }) => {
         setTokens(data);
+        toast.success("Login successful");
+      })
+      .catch(() => {
+        toast.error("Invalid credentials");
       });
   };
   return (
     <div>
-      <div className="flex gap-5 flex-col">
-        <Input label="Email" {...emailInput} />
-        <Input label="Password" type="password" {...passwordInput} />
-        <Button className="w-full" onClick={login}>
-          Login
-        </Button>
-      </div>
+      <form onSubmit={(e) => login(e)} className="flex gap-5 flex-col">
+        <Input autoComplete="username" label="Email" {...emailInput} />
+        <Input
+          autoComplete="current-password"
+          label="Password"
+          type="password"
+          {...passwordInput}
+        />
+        <Button className="w-full">Login</Button>
+      </form>
       <div className="flex flex-col justify-center items-center mt-5">
         <p>Don't have an account?</p>
         <Link to={"/auth/register"}>
--- Frontend/src/Router/Root.tsx
+++ Frontend/src/Router/Root.tsx
@@ -2,13 +2,19 @@ import { Outlet } from "react-router";
 import Header from "../Layout/Header";
 import Footer from "../Layout/Footer";
 import SideBar from "../Layout/SideBar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// BUG: Forgot to import CSS file for toastify
 
 const Root = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen min-w-[375px">
+      <ToastContainer />
       <Header />
       <SideBar />
-      <Outlet />
+      <div className="flex-1 w-full flex">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
