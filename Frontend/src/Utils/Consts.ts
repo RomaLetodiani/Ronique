ea8fb26ace86d7f607758da95d1fb5fc1af62094
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-01 22:32:24 +0400

    Texts

---
+++ Frontend/src/Utils/Consts.ts
@@ -0,0 +1,93 @@
+import {
+  facebook,
+  gmail,
+  googleMaps,
+  linkedIn,
+  phone,
+  twitter,
+} from "../Components/Shared/Assets/icons";
+
+const navBarText = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Courses",
+    path: "/courses",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  },
+  {
+    name: "FAQ",
+    path: "/faq",
+  },
+];
+
+const FooterTextLeft = [
+  {
+    text: "Ronique@Gmail.com",
+    alt: "Gmail",
+    icon: gmail,
+  },
+  {
+    text: "+1 234 567 890",
+    alt: "Phone Number",
+    icon: phone,
+  },
+  {
+    text: "Somewhere in the world",
+    alt: "Google Maps",
+    icon: googleMaps,
+  },
+];
+
+const FooterTextRight = {
+  firstHalf: navBarText,
+  secondHalf: [
+    {
+      name: "About Us",
+      path: "/about",
+    },
+    {
+      name: "Company",
+      path: "/about",
+    },
+    {
+      name: "Achievements",
+      path: "/about",
+    },
+    {
+      name: "Our Goals",
+      path: "/about",
+    },
+  ],
+};
+
+const Socials = [
+  {
+    alt: "Facebook",
+    icon: facebook,
+    href: "https://www.facebook.com",
+  },
+  {
+    alt: "Twitter",
+    icon: twitter,
+    href: "https://www.facebook.com",
+  },
+  {
+    alt: "LinkedIn",
+    icon: linkedIn,
+    href: "https://www.facebook.com",
+  },
+];
+
+const allRightsReserved = "2024 Ronique. All Rights Reserved";
+
+export { navBarText, FooterTextLeft, FooterTextRight, Socials, allRightsReserved };
