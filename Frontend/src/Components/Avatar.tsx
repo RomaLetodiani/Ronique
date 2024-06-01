263f2877688ad38dc61a03fe680910bdbe0976ea
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-01 22:42:26 +0400

    Create Avatar.tsx

---
+++ Frontend/src/Components/Avatar.tsx
@@ -0,0 +1,57 @@
+import { twMerge } from "tailwind-merge";
+import { Role } from "../Types/User.interface";
+
+type avatarProps = {
+  firstName: string;
+  lastName: string;
+  role: Role;
+  imageOnly?: boolean;
+  bgColor?: string;
+  textColor?: string;
+  image?: string;
+  imageSize?: string;
+};
+
+const Avatar = ({
+  firstName,
+  lastName,
+  role,
+  imageOnly = false,
+  bgColor = "bg-primary",
+  textColor = "text-white",
+  image,
+  imageSize = "h-10 w-10",
+}: avatarProps) => {
+  const fullName = firstName + " " + lastName;
+
+  return (
+    <div className="flex gap-3">
+      {image ? (
+        <div>
+          <img src={image} alt={fullName} />
+        </div>
+      ) : (
+        <div
+          className={twMerge(
+            "flex justify-center items-center border rounded-full ",
+            imageSize,
+            bgColor,
+            textColor
+          )}
+        >
+          {firstName.slice(0, 1).toUpperCase() + lastName.slice(0, 1).toUpperCase()}
+        </div>
+      )}
+      {!imageOnly && (
+        <div>
+          <h3 className="text-sm">
+            {fullName.length > 15 ? fullName.slice(0, 15) + "..." : fullName}
+          </h3>
+          <p className="text-xs">{role === Role.ADMIN ? "Admin" : "Customer"}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Avatar;
