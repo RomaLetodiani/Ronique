7f5be06e6b49914a65f3a37683016a934f171253
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-01 22:32:04 +0400

    assets

--- Frontend/src/Components/Shared/Assets/icons.ts
+++ Frontend/src/Components/Shared/Assets/icons.ts
@@ -1,3 +1,8 @@
-import copyIcon from "../../Assets/Icons/copy-svgrepo-com.svg";
+import facebook from "../../../Assets/Icons/facebook.svg";
+import googleMaps from "../../../Assets/Icons/googleMaps.svg";
+import gmail from "../../../Assets/Icons/gmail.svg";
+import linkedIn from "../../../Assets/Icons/linkedin.svg";
+import twitter from "../../../Assets/Icons/twitter.svg";
+import phone from "../../../Assets/Icons/phone.svg";
 
-export { copyIcon };
+export { facebook, googleMaps, gmail, linkedIn, twitter, phone };
