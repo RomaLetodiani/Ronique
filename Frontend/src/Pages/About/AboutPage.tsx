78bb8d4ec93b6d1a1fe70fb4059b5489d48fe6ab
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-06 22:22:56 +0400

    Update AboutPage.tsx

--- Frontend/src/Pages/About/AboutPage.tsx
+++ Frontend/src/Pages/About/AboutPage.tsx
@@ -1,5 +1,26 @@
+import SectionWrapper from "../../Components/SectionWrapper";
+import Achievements from "./Views/Achievements";
+import BottomFooter from "./Views/BottomFooter";
+import Goals from "./Views/Goals";
+
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <SectionWrapper maxWidth="max-w-[1440px]">
+      <div className="flex flex-col gap-5 p-8">
+        <div className="flex justify-between flex-col md:flex-row md:gap-40">
+          <h1 className="text-4xl font-bold text-center">About Us</h1>
+          <p className="max-w-96">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, ut quod dignissimos
+            voluptatum odio ducimus id consequatur sint, illum commodi corporis iure suscipit enim
+            repellendus distinctio nostrum, non quidem quisquam.
+          </p>
+        </div>
+        <Achievements />
+        <Goals />
+        <BottomFooter />
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default AboutPage;
