330e460d086e70c384fe4f5a274f1e6ac074c240
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-09 00:44:55 +0400

    Update Profile.tsx

--- Frontend/src/Pages/Profile/Profile.tsx
+++ Frontend/src/Pages/Profile/Profile.tsx
@@ -1,17 +1,37 @@
 import { useEffect } from "react";
 import userServices from "../../Services/UserServices";
 import authStore from "../../Stores/Auth.store";
+import SideBar from "./SideBar";
+import { Outlet, useNavigate } from "react-router";
+import UserProfile from "./UserProfile";
+import SectionWrapper from "../../Components/SectionWrapper";
 
 const Profile = () => {
   const { fullUser, setFullUser } = authStore();
-  console.log("🚀 ~ Profile ~ fullUser:", fullUser);
+
+  const navigate = useNavigate();
   useEffect(() => {
-    userServices.getCurrent().then(({ data }) => {
-      setFullUser(data);
-    });
+    userServices
+      .getCurrent()
+      .then(({ data }) => {
+        setFullUser(data);
+      })
+      .catch(() => {
+        if (!fullUser) {
+          navigate("/auth");
+        }
+      });
   }, []);
 
-  return <div>Profile</div>;
+  return (
+    <div className="flex w-full">
+      <SideBar role={fullUser?.role} />
+      <SectionWrapper className="flex-1">
+        <Outlet />
+      </SectionWrapper>
+      <UserProfile user={fullUser} />
+    </div>
+  );
 };
 
 export default Profile;
