28ecc03d0560984d59bf33d29d05c76b40a933ca
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-09 00:41:40 +0400

    danger

--- Frontend/src/Components/UI/Button.tsx
+++ Frontend/src/Components/UI/Button.tsx
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 interface ButtonI extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   className?: string;
-  btnType?: "primary" | "secondary";
+  btnType?: "primary" | "secondary" | "danger";
 }
 
 const Button = ({ children, className, btnType = "primary", ...rest }: ButtonI) => {
@@ -14,6 +14,7 @@ const Button = ({ children, className, btnType = "primary", ...rest }: ButtonI) 
       className={twMerge(
         "rounded-lg cursor-pointer px-5 py-2 shadow-lg hover:opacity-70",
         btnType === "primary" ? "bg-primary text-white" : "bg-secondary-200 text-primary",
+        btnType === "danger" && "bg-danger text-white",
         className
       )}
     >
