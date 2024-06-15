81230089052db1cb1f094a89cd6de2b6cb00c15f
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-15 21:57:29 +0400

    Refactored SideBar

--- Frontend/src/Pages/Profile/Profile.tsx
+++ Frontend/src/Pages/Profile/Profile.tsx
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import userServices from "../../Services/UserServices";
 import authStore from "../../Stores/Auth.store";
 import SideBar from "./SideBar";
 import { Outlet, useNavigate } from "react-router";
 import UserProfile from "./UserProfile";
 import SectionWrapper from "../../Components/SectionWrapper";
+import useMediaQuery from "../../Hooks/UseMediaQuery";
+import { twMerge } from "tailwind-merge";
 
 const Profile = () => {
   const { fullUser, setFullUser } = authStore();
+  const isNotMobile = useMediaQuery("(min-width: 768px)");
+  const [open, setOpen] = useState(false);
 
   const navigate = useNavigate();
   useEffect(() => {
@@ -24,12 +28,12 @@ const Profile = () => {
   }, []);
 
   return (
-    <div className="flex w-full">
-      <SideBar role={fullUser?.role} />
-      <SectionWrapper className="flex-1">
+    <div className="flex flex-col md:flex-row">
+      <SideBar open={open} setOpen={setOpen} isMobile={!isNotMobile} role={fullUser?.role} />
+      <SectionWrapper className={twMerge("flex-1", isNotMobile ? "mt-0" : "mt-32")}>
         <Outlet />
       </SectionWrapper>
-      <UserProfile user={fullUser} />
+      {isNotMobile && <UserProfile user={fullUser} />}
     </div>
   );
 };
--- Frontend/src/Pages/Profile/SideBar.tsx
+++ Frontend/src/Pages/Profile/SideBar.tsx
@@ -2,34 +2,61 @@ import { Link } from "react-router-dom";
 import { Role } from "../../Types/User.interface";
 import { isAdmin } from "../../Utils/Claims";
 import Logo from "../../Components/Logo";
+import Burger from "../../Components/Burger";
+import UserAvatar from "../../Components/UserAvatar";
+import { twMerge } from "tailwind-merge";
 
-const SideBar = ({ role }: { role?: Role }) => {
+type SideBarProps = {
+  role?: Role;
+  isMobile: boolean;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const SideBar = ({ role, isMobile, open, setOpen }: SideBarProps) => {
   return (
-    <div className="border-r border-secondary-200 p-5">
-      <Logo size="100" />
+    <>
+      {isMobile && (
+        <div className="absolute z-50 top-[170px] w-full">
+          {/*   TODO: user Information */}
+          {/* <UserAvatar /> */}
+          <Burger open={open} setOpen={setOpen} />
+        </div>
+      )}
 
-      <div>
-        <p>OverView</p>
-        <ul>
-          <li>Dashboard</li>
-          <li>Inbox</li>
-          <li>Task</li>
-          {isAdmin(role) && (
-            <li>
-              <Link to={"admin"}>Admin Panel</Link>
-            </li>
-          )}
-        </ul>
-      </div>
+      <div
+        className={twMerge(
+          "p-5 top-0 justify-between items-center gap-5 bg-primary-500 w-64 z-40 bg-white transform transition-transform duration-300",
+          !isMobile && "border-r h-full border-secondary-200",
+          isMobile && "fixed w-full h-screen flex flex-col",
+          isMobile && (open ? "translate-y-0" : "-translate-y-full")
+        )}
+      >
+        <Logo size="100" />
 
-      <div>
-        <p>Setting</p>
-        <ul>
-          <li>Setting</li>
-          <li>Log Out</li>
-        </ul>
+        <div>
+          <p>OverView</p>
+          <ul>
+            <li>Dashboard</li>
+            <li>Inbox</li>
+            <li>Task</li>
+            {isAdmin(role) && (
+              <li>
+                <Link to={"admin"}>Admin Panel</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+
+        <div>
+          <p>Setting</p>
+          <ul>
+            <li>Setting</li>
+            <li>Log Out</li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
