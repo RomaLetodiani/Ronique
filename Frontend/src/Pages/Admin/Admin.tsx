ce23cf6f4901ff0ce788efc5d61ec2a63c6cab6e
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-21 20:35:05 +0400

    test commit

--- Frontend/src/Pages/Admin/Admin.tsx
+++ Frontend/src/Pages/Admin/Admin.tsx
@@ -1,12 +1,6 @@
 import { Outlet } from "react-router";
 
 const Admin = () => {
-  const arr = [
-    "aaaaaaaa", 
-    "bbbbbbbb", 
-    "cccccccc", 
-    "dddddddd", 
-    "eeeeeeee"];
   return (
     <div>
       <h1>Admin Panel</h1>
