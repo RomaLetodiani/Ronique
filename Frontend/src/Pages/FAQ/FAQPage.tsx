73caac1921325de3c01709ca25d21a5edad39de0
RomanLetodiani <roman.letodiani03@gmail.com>
2024-06-25 20:37:50 +0400

    FAQ Page

--- Frontend/src/Pages/FAQ/FAQPage.tsx
+++ Frontend/src/Pages/FAQ/FAQPage.tsx
@@ -1,5 +1,18 @@
+import SectionWrapper from "../../Components/SectionWrapper";
+import { FAQs } from "../../Utils/Consts";
+import Question from "./Question";
+
 const FAQPage = () => {
-  return <div>FAQPage</div>;
+  return (
+    <SectionWrapper maxWidth="max-w-[1440px]" className="flex items-center flex-col p-5">
+      <h1 className="text-2xl mb-5">Frequently Asked Questions</h1>
+      <div className="flex flex-col w-full border rounded-2xl overflow-hidden">
+        {FAQs.map((faq, index) => (
+          <Question key={index} question={faq.question} answer={faq.answer} />
+        ))}
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default FAQPage;
