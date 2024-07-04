b85826cf7567ee76c3fea0addea0f0b05392d7b0
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-04 20:42:58 +0400

    Added initialFilterParams

--- Frontend/src/Stores/Filtered.store.ts
+++ Frontend/src/Stores/Filtered.store.ts
@@ -1,19 +1,21 @@
 import { create } from "zustand";
 import { ProductFilterI } from "../Types/Product.interface";
 
+export const initialFilterParams: ProductFilterI = {
+  page: 1,
+  pageSize: 12,
+  minPrice: 0,
+  maxPrice: 1000,
+  onlySales: false,
+};
+
 interface FilterProductsStoreI {
   filterParams: ProductFilterI;
   setFilterParams: (params: ProductFilterI) => void;
 }
 
 const filteredProductsStore = create<FilterProductsStoreI>()((set) => ({
-  filterParams: {
-    page: 1,
-    pageSize: 12,
-    minPrice: 0,
-    maxPrice: 1000,
-    onlySales: false,
-  },
+  filterParams: initialFilterParams,
   setFilterParams: (params) =>
     set((state) => ({ filterParams: { ...state.filterParams, ...params } })),
 }));
