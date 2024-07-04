aaba3225d68b7fa9721138b30df6e83ea4c58033
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-04 22:14:23 +0400

    Mentors Images

---
+++ Frontend/src/Components/Shared/Assets/Mentors.ts
@@ -0,0 +1,33 @@
+import mentor0 from "../../../Assets/Images/Mentors/01.jpg";
+import mentor1 from "../../../Assets/Images/Mentors/02.jpg";
+import mentor2 from "../../../Assets/Images/Mentors/03.jpg";
+import mentor3 from "../../../Assets/Images/Mentors/04.jpg";
+import mentor4 from "../../../Assets/Images/Mentors/05.jpg";
+import mentor5 from "../../../Assets/Images/Mentors/06.jpg";
+import mentor6 from "../../../Assets/Images/Mentors/07.jpg";
+import mentor7 from "../../../Assets/Images/Mentors/08.jpg";
+import mentor8 from "../../../Assets/Images/Mentors/09.jpg";
+import mentor9 from "../../../Assets/Images/Mentors/10.jpg";
+import mentor10 from "../../../Assets/Images/Mentors/11.jpg";
+import mentor11 from "../../../Assets/Images/Mentors/12.jpg";
+import mentor12 from "../../../Assets/Images/Mentors/13.jpg";
+import mentor13 from "../../../Assets/Images/Mentors/14.jpg";
+import mentor14 from "../../../Assets/Images/Mentors/15.jpg";
+
+export {
+  mentor0,
+  mentor1,
+  mentor2,
+  mentor3,
+  mentor4,
+  mentor5,
+  mentor6,
+  mentor7,
+  mentor8,
+  mentor9,
+  mentor10,
+  mentor11,
+  mentor12,
+  mentor13,
+  mentor14,
+};
