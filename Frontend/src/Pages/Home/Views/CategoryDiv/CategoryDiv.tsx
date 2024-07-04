3820649949f112631749e58a6da25c62451d595b
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-04 21:06:00 +0400

    CategoryDiv

--- Frontend/src/Pages/Home/Views/CategoryDiv/CategoryDiv.tsx
+++ Frontend/src/Pages/Home/Views/CategoryDiv/CategoryDiv.tsx
@@ -1,7 +1,25 @@
-import SectionWrapper from "../../../../Components/SectionWrapper";
+import { Link } from "react-router-dom";
+import categoryStore from "../../../../Stores/Category.store";
+import filteredStore from "../../../../Stores/Filtered.store";
 
 const CategoryDiv = () => {
-  return <SectionWrapper className="bg-red-200">CategoryDiv</SectionWrapper>;
+  const { categories } = categoryStore();
+  const { setFilterParams } = filteredStore();
+
+  const handleCategoryClick = (category: string) => {
+    setFilterParams({ categoryName: category });
+  };
+  return (
+    <div className="px-5 py-2 flex gap-5 overflow-auto w-full">
+      {categories.map((category) => (
+        <Link onClick={() => handleCategoryClick(category.name)} to="/courses" key={category.id}>
+          <div className="py-1 px-3 rounded-lg bg-black/10">
+            <p className="text-nowrap">{category.name}</p>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default CategoryDiv;
