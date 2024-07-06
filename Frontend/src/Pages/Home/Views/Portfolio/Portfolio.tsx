7755a75cde97f2dd81ccd456ce9357a0fcac9aed
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-06 22:08:26 +0400

    Portfolio Section

--- Frontend/src/Pages/Home/Views/Portfolio/Portfolio.tsx
+++ Frontend/src/Pages/Home/Views/Portfolio/Portfolio.tsx
@@ -1,10 +1,31 @@
+import CoursesCard from "../../../../Components/Courses/CoursesCard";
 import SectionWrapper from "../../../../Components/SectionWrapper";
+import productStore from "../../../../Stores/Product.store";
 
+// TODO: FINISH THIS and REFACTOR IT
 const Portfolio = () => {
+  const { products } = productStore();
   return (
-    <div className="bg-black/10">
-      <SectionWrapper>Portfolio</SectionWrapper>
-    </div>
+    products && (
+      <div className="bg-gradient bg-cover bg-center">
+        <SectionWrapper className="py-10">
+          <div className="text-center mb-10">
+            <h3 className="text-4xl font-bold">Trending Courses</h3>
+            <p>Let's Join our Trending Courses, the knowledge will definitely be useful to you</p>
+          </div>
+
+          <div className="flex flex-wrap justify-center gap-5">
+            {products.slice(0, 3).map((product) => (
+              <CoursesCard
+                key={product.id}
+                className="flex-1 min-w-[345px] max-w-[400px] lg:min-w-0"
+                {...product}
+              />
+            ))}
+          </div>
+        </SectionWrapper>
+      </div>
+    )
   );
 };
 
