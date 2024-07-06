c86acb435d196c8178202907cd9b1152eae7d6d4
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-06 20:55:58 +0400

    Home Services Section

--- Frontend/src/Pages/Home/Views/Services/Services.tsx
+++ Frontend/src/Pages/Home/Views/Services/Services.tsx
@@ -1,5 +1,14 @@
+import SectionWrapper from "../../../../Components/SectionWrapper";
+import RenderServicesCards from "./RenderServicesCards";
+import RenderServiceTexts from "./RenderServiceTexts";
+
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <SectionWrapper className="flex text-secondary-500 flex-col md:flex-row gap-10 py-10">
+      <RenderServiceTexts />
+      <RenderServicesCards />
+    </SectionWrapper>
+  );
 };
 
 export default Services;
