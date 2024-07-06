f8fdef94789507e1499986baf06a0cc7f4e5244f
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-06 20:51:37 +0400

    Refactored Card Component

--- Frontend/src/Components/Card.tsx
+++ Frontend/src/Components/Card.tsx
@@ -1,14 +1,16 @@
+import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 import { twMerge } from "tailwind-merge";
 
 export type CardProps = {
   title: string;
   description: string;
-  icon: string;
+  icon?: string;
+  iconComponent?: ReactNode;
   href?: string;
   className?: string;
-  direction: "flex-row" | "flex-col" | "flex-row-reverse" | "flex-col-reverse";
-  align:
+  direction?: "flex-row" | "flex-col" | "flex-row-reverse" | "flex-col-reverse";
+  align?:
     | "items-center"
     | "items-start"
     | "items-end"
@@ -17,15 +19,37 @@ export type CardProps = {
     | "items-baseline";
 };
 
-const Card = ({ title, description, icon, href = "", className, direction, align }: CardProps) => {
+const Card = ({
+  title,
+  description,
+  icon,
+  iconComponent,
+  href = "",
+  className,
+  direction = "flex-row",
+  align = "items-start",
+}: CardProps) => {
   return (
     <Link to={href}>
-      <div className={twMerge("flex flex-1", direction, align, className)}>
-        <div className="w-10">
-          <img src={icon} alt={title} />
-        </div>
+      <div
+        className={twMerge(
+          "h-full p-5",
+          "flex gap-5 lg:items-start",
+          "border rounded-xl shadow-md",
+          direction,
+          align,
+          className
+        )}
+      >
+        {icon ? (
+          <div className="w-10">
+            <img src={icon} alt={title} />
+          </div>
+        ) : (
+          iconComponent
+        )}
         <div>
-          <h1>{title}</h1>
+          <h1 className="text-xl font-semibold">{title}</h1>
           <p>{description}</p>
         </div>
       </div>
