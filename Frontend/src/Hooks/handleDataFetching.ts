44781f1872268f4550125c97d7edcd887f0f5891
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-11 22:22:38 +0400

    FetchData

--- Frontend/src/Hooks/handleDataFetching.ts
+++ Frontend/src/Hooks/handleDataFetching.ts
@@ -5,6 +5,10 @@ import { toast } from "react-toastify";
 import categoryStore from "../Stores/Category.store";
 import productStore from "../Stores/Product.store";
 import filteredStore from "../Stores/Filtered.store";
+import wishlistProductStore from "../Stores/Wishlist.store";
+import WishListServices from "../Services/WishlistServices";
+import cartProductStore from "../Stores/Cart.store";
+import cartServices from "../Services/CartServices";
 
 const handleDataFetching = () => {
   const { filterParams } = filteredStore();
@@ -12,7 +16,9 @@ const handleDataFetching = () => {
   const { loadingProducts, setProducts, setLoadingProducts, setTotalProducts } = productStore();
 
   const { setCategories, loadingCategories, setLoadingCategories } = categoryStore();
-
+  const { setWishlistProducts, loadingWishlistProducts, setLoadingWishlistProducts } =
+    wishlistProductStore();
+  const { setCartProducts, loadingCartProducts, setLoadingCartProducts } = cartProductStore();
   // INFO: This is a custom hook to fetch data from the server and store it in the global store.
 
   // INFO: Fetch Products
@@ -33,20 +39,31 @@ const handleDataFetching = () => {
     }
   }, [filterParams]);
 
-  // INFO: Fetch Categories
+  // INFO: Fetch Categories and Wishlist Products
   useEffect(() => {
-    if (!loadingCategories) {
+    if (!loadingCategories && !loadingWishlistProducts && !loadingCartProducts) {
       setLoadingCategories(true);
-      categoryServices
-        .allCategories()
-        .then(({ data }) => {
-          setCategories(data);
+      setLoadingWishlistProducts(true);
+      setLoadingCartProducts(true);
+      Promise.all([
+        categoryServices.allCategories(),
+        WishListServices.allProducts(),
+        cartServices.allCartProducts(),
+      ])
+        .then(([categories, wishlistProducts, cartProducts]) => {
+          setCategories(categories.data);
+          setWishlistProducts(wishlistProducts.data);
+          setCartProducts(cartProducts.data);
         })
         .catch((error) => {
-          console.error("❌ ~ categoryServices.allCategories ~ error:", error.message);
+          console.error("❌ fetchCategories ~ error:", error.message);
           toast.error("Error while getting categories Data!");
         })
-        .finally(() => setLoadingCategories(false));
+        .finally(() => {
+          setLoadingCategories(false);
+          setLoadingWishlistProducts(false);
+          setLoadingCartProducts(false);
+        });
     }
   }, []);
 };
