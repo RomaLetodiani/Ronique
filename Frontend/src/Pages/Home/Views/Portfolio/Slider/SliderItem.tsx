cb57ac3b5e555ec13d3317bad3692abe118ebc76
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-11 20:59:01 +0400

    Update SliderItem.tsx

--- Frontend/src/Pages/Home/Views/Portfolio/Slider/SliderItem.tsx
+++ Frontend/src/Pages/Home/Views/Portfolio/Slider/SliderItem.tsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
 import { ProductI } from "../../../../../Types/Product.interface";
 import { renderImage } from "../../../../../Utils/helpers";
+import CartWishlistButton from "../../../../../Components/CartWishlistButton/CartWishlistButton";
+import { handleActions } from "../../../../../Components/CartWishlistButton/handler";
 
 const SliderItem = ({ product }: { product: ProductI }) => {
   return (
-    <Link to={`/courses/${product.id}`}>
-      <div className="text-center h-full flex flex-col justify-between p-3 border border-secondary-500 rounded-xl">
+    <div className="text-center h-full flex flex-col justify-between p-3 border border-secondary-500 rounded-xl">
+      <Link className="h-full" to={`/courses/${product.id}`}>
         <div className="overflow-hidden rounded-lg">
           <img src={renderImage(product.image)} alt={product.title} />
         </div>
         <h4>{product.title}</h4>
+      </Link>
+      <div className="flex justify-between gap-3">
+        <CartWishlistButton
+          onClick={() => handleActions(product.id, "wishlist")}
+          path="/"
+          isWishlist
+        />
+        <CartWishlistButton onClick={() => handleActions(product.id, "cart")} path="/" isCart />
       </div>
-    </Link>
+    </div>
   );
 };
 
