4f057234bc6223465af654516e6156d9eddff9a5
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-13 21:14:01 +0400

    Courses

--- Frontend/src/Pages/Courses/Courses.tsx
+++ Frontend/src/Pages/Courses/Courses.tsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import SectionWrapper from "../../Components/SectionWrapper";
+import useMediaQuery from "../../Hooks/UseMediaQuery";
+import Filters from "./Filters/Filters";
+import RenderCourses from "./RenderCourses";
+
 const Courses = () => {
-  return <div>Courses</div>;
+  const isNotMobile = useMediaQuery("(min-width: 1000px)");
+  const [open, setOpen] = useState(false);
+  return (
+    <SectionWrapper className="grid grid-cols-[auto_1fr] min-h-screen relative overflow-hidden">
+      <Filters isMobile={!isNotMobile} open={open} setOpen={setOpen} />
+      <RenderCourses isMobile={!isNotMobile} />
+    </SectionWrapper>
+  );
 };
 
 export default Courses;
