d89d005218e7d1881bbd5347d55bfe80d6492d59
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-20 22:36:05 +0400

    UI index

---
+++ Frontend/src/Components/UI/index.ts
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import Input from "./Input";
+import Selector from "./Selector";
+import CheckBox from "./CheckBox";
+import Pagination from "./Pagination/Pagination";
+
+export { Button, Input, Selector, CheckBox, Pagination };
