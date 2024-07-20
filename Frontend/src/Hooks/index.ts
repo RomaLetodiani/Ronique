a8d5ef867c6439ad57a6ee6669ab9402f74720fb
RomanLetodiani <roman.letodiani03@gmail.com>
2024-07-20 22:35:43 +0400

    Hooks index

---
+++ Frontend/src/Hooks/index.ts
@@ -0,0 +1,25 @@
+import handleCartItems from "./handleCartItems";
+import handleCartProductsFetcher from "./handleCartProductsFetcher";
+import handleDataFetching from "./handleDataFetching";
+import handlerWishlist from "./handlerWishlist";
+import useClickInside from "./useClickInside";
+import useClickOutside from "./useClickOutside";
+import useDebounce from "./useDebounce";
+import useInput from "./useInput";
+import useMediaQuery from "./UseMediaQuery";
+import useScrollPosition from "./useScrollPosition";
+import useTimeout from "./useTimeout";
+
+export {
+  useInput,
+  useTimeout,
+  useDebounce,
+  useMediaQuery,
+  useClickInside,
+  useClickOutside,
+  handleCartItems,
+  handlerWishlist,
+  useScrollPosition,
+  handleDataFetching,
+  handleCartProductsFetcher,
+};
